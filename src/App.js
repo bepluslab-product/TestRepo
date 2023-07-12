676fde6bc54182a656830fd65af40e57cf80078c
oye <oyes78k@gmail.com>
2023-07-12 13:25:50 +0900

    keycloak.js test

--- src/App.js
+++ src/App.js
@@ -2,15 +2,79 @@ import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 import {useEffect} from "react";
-
+import Keycloak from 'keycloak-js'
 
 
 function App() {
+
+
+  let initOptions = {
+    url: 'http://14.32.42.32:8088/auth',
+    realm: 'Bumin-realm',
+    clientId: 'gateway',
+    onLoad: 'login-required',
+  };
+  
+  let keycloak = new Keycloak(initOptions);
+
+  
+  init();
+  
+  function init() {
+    keycloak.init({
+      onLoad: initOptions.onLoad,
+    }).then(auth => {
+      const ONE_MINUTE = 60000;
+    
+      if (!auth) {
+        window.location.reload();
+      } else {
+        console.info('Auth ok');
+      }
+
+      console.log('keycloak.token')
+      console.log(keycloak.token)
+    
+    
+      localStorage.setItem('vue-token', keycloak.token);
+      localStorage.setItem('vue-refresh-token', keycloak.refreshToken);
+    
+      window.setTimeout(refreshToken.bind(null, keycloak), ONE_MINUTE);
+    }).catch(() => {
+      console.error('Auth Fail');
+    })
+  }
+  
+  function refreshToken() {
+    keycloak.updateToken(70).success(refreshed => {
+      if (refreshed) {
+        successRefresh(refreshed);
+      } else {
+        warnRefresh();
+      }
+    }).error(errorRefresh);
+  }
+  
+  function successRefresh(refreshed) {
+    console.debug('Token refreshed ' + refreshed);
+  }
+  
+  function warnRefresh() {
+    //console.warn(`Token not refreshed, valid for 
+   // ${Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000)} seconds`);
+  }
+  
+  function errorRefresh() {
+    console.error('Failed to refresh token');
+  }
+
+
   useEffect(() => {
 
-    keycloakTest();
+    //keycloakTest();
   
   });
+  
   
   const keycloakTest = async() => {
     const res = await axios.get('/admin/realms')
